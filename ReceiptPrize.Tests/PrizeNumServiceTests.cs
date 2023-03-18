80054f90a8c293710eb05791c81b4ec8f7f09b7a
KyouShou <j_123321@hotmail.com>
2023-03-19 05:09:16 +0800

    重構

--- ReceiptPrize.Tests/PrizeNumServiceTests.cs
+++ ReceiptPrize.Tests/PrizeNumServiceTests.cs
@@ -27,17 +27,33 @@ namespace ReceiptPrize.Tests
         [Test]
         public void Test_GetPrizeNumber_FromMinistryOfFinance()
         {
-            IPrizeNumRepository ministryOfFinancePrizeNumRepository = new MinistryOfFinancePrizeNumRepository();
-            _prizeNumService = new PrizeNumService(ministryOfFinancePrizeNumRepository);
+            List<string> prizeList = Fetch_PrizeList_From_MinistryOfFinance();
 
-            var prizeList = _prizeNumService.GetPrizeNumber();
+            Check_Is_NunInList_Legal(prizeList);
 
+            Check_Is_PrizeList_Not_Empty(prizeList);
+        }
+
+        private static void Check_Is_PrizeList_Not_Empty(List<string> prizeList)
+        {
+            Assert.IsTrue(prizeList.Count > 0);
+        }
+
+        private void Check_Is_NunInList_Legal(List<string> prizeList)
+        {
             foreach (var num in prizeList)
             {
                 Assert.IsTrue(IsEightDigitNum(num));
             }
+        }
 
-            Assert.IsTrue(prizeList.Count > 0);
+        private List<string> Fetch_PrizeList_From_MinistryOfFinance()
+        {
+            IPrizeNumRepository ministryOfFinancePrizeNumRepository = new MinistryOfFinancePrizeNumRepository();
+            _prizeNumService = new PrizeNumService(ministryOfFinancePrizeNumRepository);
+
+            var prizeList = _prizeNumService.GetPrizeNumber();
+            return prizeList;
         }
 
         private bool IsEightDigitNum(string num)
