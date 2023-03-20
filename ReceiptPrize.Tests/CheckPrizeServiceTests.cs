c8c9009e20b975e665a62c27245f03abab932f5e
KyouShou <j_123321@hotmail.com>
2023-03-20 19:43:57 +0800

    新增測試：CheckPrizeService中獎-Fail

---
+++ ReceiptPrize.Tests/CheckPrizeServiceTests.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using ReceiptPrize.Controllers;
+using ReceiptPrize.Exceptions;
+using ReceiptPrize.Repository;
+using ReceiptPrize.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReceiptPrize.Tests
+{
+    internal class CheckPrizeServiceTests
+    {
+
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [TearDown]
+        public void End()
+        {
+
+        }
+
+        [Test]
+        public void Did_Not_Win_Returns_False()
+        {
+            var inputNum = "000";
+            var fetchPrizeServiceMock = new Mock<IFetchPrizeNumService>();
+
+            List<string> fakePrizeNumberList = new List<string>();
+            fakePrizeNumberList.Add("000");
+
+            fetchPrizeServiceMock.Setup(m => m.GetPrizeNumber()).Returns(fakePrizeNumberList);
+
+            CheckPrizeService checkPrizeService = new CheckPrizeService(fetchPrizeServiceMock.Object);
+
+            var result = checkPrizeService.Check(inputNum);
+
+            Assert.AreEqual(true , result);
+        }
+
+        //[Test]
+        //public void Win_Returns_True()
+        //{
+        //    Assert.Fail();
+        //}
+
+        //[Test]
+        //public void Input_Content_Illegal_Throws_NumberFormatErrorException()
+        //{
+        //    Assert.Fail();
+        //}
+
+        //[Test]
+        //public void Can_Not_Fetch_PrizeNumbers_Throws_FetchPrizeFailException()
+        //{
+        //    Assert.Fail();
+        //}
+    }
+}
---
+++ ReceiptPrize/Service/CheckPrizeService.cs
@@ -0,0 +1,17 @@
+﻿namespace ReceiptPrize.Service
+{
+    public class CheckPrizeService : ICheckPrizeService
+    {
+        IFetchPrizeNumService _fetchPrizeNumService;
+
+        public CheckPrizeService(IFetchPrizeNumService fetchPrizeNumService)
+        {
+            this._fetchPrizeNumService = fetchPrizeNumService;
+        }
+
+        public bool Check(string num)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
