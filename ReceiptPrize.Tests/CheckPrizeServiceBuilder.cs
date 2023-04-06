fa19f853b1d3e5d8a7d686725e77cf4202ebf97a
KyouShou <31533013+KyouShou@users.noreply.github.com>
2023-04-06 15:59:54 +0800

    refactor:新增CheckPrizeServiceBuilder類別
    問題：
    CheckPrizeService在Tests的初始化相當混亂
    
    原因：
    由於有時候要塞假資料到FetchPrizeNumService到Stub、有時要塞到Cache、有時又需要FetchPrizeNumService Throw Exception，導致CheckPrizeService的初始化過程相當複雜且多變
    
    調整項目：
    建立一個Builder處理CheckPrizeService的初始化

---
+++ ReceiptPrize.Tests/CheckPrizeServiceBuilder.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+using Moq;
+using ReceiptPrize.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReceiptPrize.Tests
+{
+    public class CheckPrizeServiceBuilder
+    {
+        private CheckPrizeService _checkPrizeService;
+        private IFetchPrizeNumService _fetchPrizeNumService;
+        private MemoryCache _memoryCache;
+
+        public CheckPrizeServiceBuilder()
+        {
+            _memoryCache = new MemoryCache(new MemoryCacheOptions());
+        }
+
+        public CheckPrizeServiceBuilder SetFakePrizeNumToFetchPrizeNumService(List<string> fakePrizeNum)
+        {
+            var stubFetchPrizeNumService = new Mock<IFetchPrizeNumService>();
+
+            stubFetchPrizeNumService.Setup(m => m.GetPrizeNumber()).Returns(fakePrizeNum);
+
+            this._fetchPrizeNumService = stubFetchPrizeNumService.Object;
+
+            return this;
+        }
+
+        public CheckPrizeServiceBuilder SetExceptionToFetchPrizeNumService(Exception e)
+        {
+            var stubFetchPrizeNumService = new Mock<IFetchPrizeNumService>();
+
+            stubFetchPrizeNumService.Setup(m => m.GetPrizeNumber()).Throws(e);
+
+            this._fetchPrizeNumService = stubFetchPrizeNumService.Object;
+
+            return this;
+        }
+
+        public CheckPrizeServiceBuilder SetFakePrizeNumToMemoryCache(List<string> fakePrizeNum)
+        {
+            _memoryCache.Set("prizeListInCache", fakePrizeNum);
+
+            return this;
+        }
+
+        public CheckPrizeService Build()
+        {
+            _checkPrizeService = new CheckPrizeService(_fetchPrizeNumService, _memoryCache);
+            return _checkPrizeService;
+        }
+    }
+
+
+}
